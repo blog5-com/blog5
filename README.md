98792c57ba82bf3024c7cf599a3d0457b3a76413
Lane Zhang <zjloveztt@gmail.com>
2025-03-05 19:10:14 +0800

    Integrated theme of `DevBlog`; Added home page and i18ned it; Allowed user to maintain the posts; Show posts in home page.

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# Blog5 - Enjoy blogging with Markdown. An open source personal blog built with Ruby on Rails 8.
+# Blog5 - Enjoy blogging with Markdown. An open source personal blog built with Ruby on Rails 8 and DevBlog theme.
 
+## Free theme 'DevBlog' installation
+
+### Download 'DevBlog Theme'
+
+- Option 1: Use Git to download.
+
+  If your network is not good, you can try `option 2`.
+
+  ```shell
+  cd _themes
+  git clone https://github.com/xriley/DevBlog-Theme.git
+  ```
+
+- Option 2: Download zip file and unzip it to `/_themes` folder.
+  1. The zip file is at:
+      - GitHub: [DevBlog-Theme zip](https://github.com/xriley/DevBlog-Theme/archive/refs/heads/master.zip)
+      - Or official website: [DevBlog Theme](https://themes.3rdwavemedia.com/bootstrap-templates/personal/devblog-free-bootstrap-5-blog-template-for-developers/).
+  2. Copy `DevBlog-Theme-master.zip` into `/_themes`.
+  3. Run `unzip DevBlog-Theme-master.zip`.
+  4. Run `mv DevBlog-Theme-master DevBlog-Theme` to rename it.
+
+### Install 'DevBlog Theme'
+
+Run:
+
+```shell
+cd _themes # if not in this folder
+sh install-theme-devblog.sh
+```
+
+## TODO
+* favicon
+
+[comment]: # (This two files are not really used: [conflict-detection.js](../../../github/DevBlog-Theme/assets/fontawesome/js/conflict-detection.js) [v4-shims.js](../../../github/DevBlog-Theme/assets/fontawesome/js/v4-shims.js))
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,68 @@
+class PostsController < ApplicationController
+  allow_unauthenticated_access
+
+  before_action :set_post, only: %i[ show edit update destroy ]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+  end
+
+  # POST /posts
+  def create
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: "Post was successfully created." }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /posts/1
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @post, notice: "Post was successfully updated." }
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /posts/1
+  def destroy
+    @post.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to posts_path, status: :see_other, notice: "Post was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params.expect(:id))
+    end
+
+    # Only allow a list of trusted parameters through.
+    def post_params
+      params.expect(post: [ :title, :content ])
+    end
+end
